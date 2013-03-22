d74e8c7517fa95feb2bd1e5a491d68cb427fbbba
Jason Jaeger <jason@greengeckodesign.com>
2013-03-22 14:24:32 -0400

    should be working now

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+console.log('beep');
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello Werld.');
 });
+
+app.listen(process.env.PORT);
+console.log('Listening on port '+process.env.PORT);
